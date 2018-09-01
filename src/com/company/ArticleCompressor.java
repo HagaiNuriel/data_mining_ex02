c78f2711b19b8875de2a0ccf55dfc27981a40f1e
mediakeys <qarnddemand@gmail.com>
2018-09-01 20:01:48 +0300

    refactor

--- src/com/company/ArticleCompressor.java
+++ src/com/company/ArticleCompressor.java
@@ -13,117 +13,81 @@ import java.util.stream.Stream;
 
 public class ArticleCompressor {
 
-
+    private static final String whitespaceDelimeter = "\\s+";
     private static String path = "Resources";
 
-    public  static void CompressArticles(String path){
+    public  static void compressArticles(String path){
         int currIndex = 0;
         List<ArticleLine> articleLines = new ArrayList<>();
         try (Stream<Path> files = Files.list(Paths.get(path))) {
-            long uniquefileCount = (files.count() - 1)/3;
+            long uniqueFileCount = (files.count() - 1)/3;
              List<Path> allFiles = files.collect(Collectors.toList());
 
-            for (long i=0; i<uniquefileCount;i++)
+            for (long i=0; i<uniqueFileCount;i++)
             {
-                articleLines.addAll(CompressFileTrio(allFiles.subList(currIndex, currIndex + 2)));
-
+                articleLines.addAll(compressFileTrio(allFiles, currIndex));
                 currIndex += 3;
             }
 
-
+            //TODO: all lines are aggregated, create new files from articleLines
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
-
-
     }
-  private static  boolean isTag(String word) {
 
-
-          for (e_tagType c : e_tagType.values()) {
-              if (c.name().equals(word)) {
-                  return true;
-              }
+    private static  boolean isTag(String word) {
+      for (e_tagType c : e_tagType.values()) {
+          if (c.name().equals(word)) {
+              return true;
           }
-
-          return false;
       }
 
+      return false;
+    }
 
-    private static Collection<? extends ArticleLine> CompressFileTrio(List<Path> paths) throws IOException {
+
+    private static Collection<? extends ArticleLine> compressFileTrio(List<Path> paths, Integer index) throws IOException {
         List<ArticleLine> articleLines = new ArrayList<>();
-        ArticleLine articleLine = new ArticleLine();
-        FileInputStream firstFileStream = new FileInputStream( paths.get(0).getFileName().toString());
-        FileInputStream seocondFileStream =  new FileInputStream( paths.get(1).getFileName().toString());
-        FileInputStream thirdFileStream = new FileInputStream( paths.get(2).getFileName().toString());
-        DataInputStream in = new DataInputStream(firstFileStream);
-        BufferedReader br = new BufferedReader(new InputStreamReader(in));
-        String strLine;
+        ArticleLine articleLine;
+        FileInputStream firstFileStream = new FileInputStream(paths.get(index).getFileName().toString());
+        FileInputStream secondFileStream = new FileInputStream(paths.get(index + 1).getFileName().toString());
+        FileInputStream thirdFileStream = new FileInputStream(paths.get(index + 2).getFileName().toString());
 
-        while ((strLine = br.readLine()) != null)   {
+        DataInputStream in1 = new DataInputStream(firstFileStream);
+        BufferedReader br1 = new BufferedReader(new InputStreamReader(in1));
 
-            String[] delims = strLine.split(" ");
-            String first = delims[0];
-            System.out.println("First word: "+first);
-            if(isTag(first)) {
+        DataInputStream in2 = new DataInputStream(secondFileStream);
+        BufferedReader br2 = new BufferedReader(new InputStreamReader(in2));
+
+        DataInputStream in3 = new DataInputStream(thirdFileStream);
+        BufferedReader br3 = new BufferedReader(new InputStreamReader(in3));
+        String strLine1, strLine2, strLine3;
+
+        while ((strLine1 = br1.readLine()) != null &&
+                (strLine2 = br2.readLine()) != null &&
+                (strLine3 = br3.readLine()) != null) {
+
+            String first = strLine1.split(whitespaceDelimeter)[0];
+            System.out.println("First word: " + first);
+            articleLine = new ArticleLine();
+
+            if (isTag(first)) {
                 articleLine.addTag(e_tagType.valueOf(first));
-                articleLine.setText(filterLine(strLine));
+                articleLine.addTag(e_tagType.valueOf(strLine2.split(whitespaceDelimeter)[0]));
+                articleLine.addTag(e_tagType.valueOf(strLine3.split(whitespaceDelimeter)[0]));
+                articleLine.setText(filterLine(strLine1));
                 articleLines.add(articleLine);
-
             }
-
-
-
-
-
-
         }
 
-//        try{
-//            // Open the file that is the first
-//            // command line parameter
-//            FileInputStream fstream = new FileInputStream("RokFile.txt");
-//            // Get the object of DataInputStream
-//            DataInputStream in = new DataInputStream(fstream);
-//            BufferedReader br = new BufferedReader(new InputStreamReader(in));
-//            String strLine;
-//            //Read File Line By Line
-//            while ((strLine = br.readLine()) != null)   {
-//
-//                String[] delims = strLine.split(" ");
-//                String first = delims[0];
-//                System.out.println("First word: "+first);
-//
-//            }
-//            //Close the input stream
-//            in.close();
-//        }catch (Exception e){//Catch exception if any
-//            System.err.println("Error: " + e.getMessage());
-//        }
+        return articleLines;
     }
 
     private static String filterLine(String strLine) {
         StringBuilder stringBuilder = new StringBuilder();
 
+        //TODO: filter line and remove unwanted words
 
-    }
-
-
-}
-
-
-    public static  long CountFiles(String path){
-
-
-
-        try (Stream<Path> files = Files.list(Paths.get(path))) {
-            return files.count();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return 0;
+        return stringBuilder.toString();
     }
 }
